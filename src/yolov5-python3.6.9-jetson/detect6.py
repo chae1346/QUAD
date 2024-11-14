6e157d124b30b160ebad64e010090f268a9944b1
chae1346 <chaeeon02@gmail.com>
2024-11-14 12:22:06 +0900

    Update detect6.py

--- src/yolov5-python3.6.9-jetson/detect6.py
+++ src/yolov5-python3.6.9-jetson/detect6.py
@@ -191,6 +191,8 @@ def run(
             # Stream results
             im0 = annotator.result()
             if view_img:
+                cv2.namedWindow(str(p), cv2.WINDOW_NORMAL)  # Create a resizable window
+                cv2.resizeWindow(str(p), 1280, 720)  # Set window size
                 cv2.imshow(str(p), im0)
                 cv2.waitKey(1)  # 1 millisecond
 
@@ -228,10 +230,10 @@ def run(
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path(s)')
-    parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob, 0 for webcam')
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5n.pt', help='model path(s)')
+    parser.add_argument('--source', type=str, default=ROOT / '2', help='file/dir/URL/glob, 0 for webcam')
     parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
-    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
+    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[320], help='inference size h,w')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
     parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
